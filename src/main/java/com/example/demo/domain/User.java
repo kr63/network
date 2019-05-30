c072db4dfbf2701d5a483bc18b50bc7b39406128
Rustam Karimov <rkarimov.r@gmail.com>
2019-05-30 23:55:35 +0400

    Setup up authorization with oauth2 on backend

---
+++ src/main/java/com/example/demo/domain/User.java
@@ -0,0 +1,25 @@
+package com.example.demo.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "usr")
+@Data
+@NoArgsConstructor
+public class User {
+
+    @Id
+    private String id;
+    private String name;
+    private String userpic;
+    private String email;
+    private String gender;
+    private String locale;
+    private LocalDateTime lastVisit;
+}
