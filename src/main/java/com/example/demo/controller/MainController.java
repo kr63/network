8e0079aae1c4386ab70f9a37d48428aa23780f71
Rustam Karimov <rkarimov.r@gmail.com>
2019-05-31 17:08:43 +0400

    Refactor frontend to the module structure. Webpack setup.

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -2,6 +2,8 @@ package com.example.demo.controller;
 
 import com.example.demo.domain.User;
 import com.example.demo.repo.MessageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,6 +18,10 @@ import java.util.Map;
 public class MainController {
     private final MessageRepo messageRepo;
 
+    @Value("${spring.profiles.active}")
+    private String profile;
+
+    @Autowired
     public MainController(MessageRepo messageRepo) {
         this.messageRepo = messageRepo;
     }
@@ -25,9 +31,13 @@ public class MainController {
                        @AuthenticationPrincipal User user) {
 
         Map<Object, Object> data = new HashMap<>();
+
         data.put("profile", user);
         data.put("messages", messageRepo.findAll());
+
         model.addAttribute("frontendData", data);
+        model.addAttribute("isDevMode", "dev".equals(profile));
+
         return "index";
     }
 }
